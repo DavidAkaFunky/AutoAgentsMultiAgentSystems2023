806b520be490b52ff0c8361eac78d13eb23e9532
David Belchior <futebol4@hotmail.com>
2023-05-16 11:48:55 +0100

    Add base files

---
+++ src/exercise_3_multi_agent.py
@@ -0,0 +1,93 @@
+import argparse
+import numpy as np
+from gym import Env
+from typing import Sequence
+
+from lib import Agent
+from lib.utils import compare_results
+from lib.share_or_take import ShareOrTake
+
+from src.exercise_1_single_random_agent import RandomAgent
+from src.exercise_2_single_random_vs_greedy import GreedyAgent
+
+
+def run_multi_agent(environment: Env, agents: Sequence[Agent], n_episodes: int) -> np.ndarray:
+
+    results = np.zeros(n_episodes)
+
+    for episode in range(n_episodes):
+
+        steps = 0
+        terminals = [False for _ in range(len(agents))]
+        observations = environment.reset()
+
+        while not all(terminals):
+            steps += 1
+            actions = []
+            for agent_id, agent in enumerate(agents):
+                if terminals[agent_id]:
+                    continue
+                agent.see(observations[agent_id])
+                actions.append(agent.action())
+                
+            observations, rewards, terminals, _ = environment.step(actions)
+            for agent_id, agent in enumerate(agents):
+                if terminals[agent_id]:
+                    continue
+                agent.feedback(rewards[agent_id])
+
+        results[episode] = steps
+
+        environment.close()
+
+    return results
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--episodes", type=int, default=100)
+    opt = parser.parse_args()
+
+    # 1 - Setup the environment
+    environment = ShareOrTake(grid_shape=(7, 7), n_agents=4, n_preys=1, max_steps=100)
+
+    # 2 - Setup the teams
+    teams = {
+
+        "Random Team": [
+            RandomAgent(environment.action_space[0].n),
+            RandomAgent(environment.action_space[1].n),
+            RandomAgent(environment.action_space[2].n),
+            RandomAgent(environment.action_space[3].n),
+        ],
+
+        "Greedy Team": [
+            GreedyAgent(agent_id=0, n_agents=4),
+            GreedyAgent(agent_id=1, n_agents=4),
+            GreedyAgent(agent_id=2, n_agents=4),
+            GreedyAgent(agent_id=3, n_agents=4)
+        ],
+
+        "1 Greedy + 3 Random": [
+            GreedyAgent(agent_id=0, n_agents=4),
+            RandomAgent(environment.action_space[1].n),
+            RandomAgent(environment.action_space[2].n),
+            RandomAgent(environment.action_space[3].n)
+        ]
+    }
+
+    # 3 - Evaluate teams
+    results = {}
+    for team, agents in teams.items():
+        result = run_multi_agent(environment, agents, opt.episodes)
+        results[team] = result
+
+    # 4 - Compare results
+    compare_results(
+        results,
+        title="Teams Comparison on 'Predator Prey' Environment",
+        colors=["orange", "green", "blue"]
+    )
+
